5a437f48c5bee9caba958d0925bf30998f67a392
edwardkinglee <edward.lee3@telus.com>
2022-11-23 16:35:32 -0500

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (letters.length === 0) {
+    return "Error. Please enter valid data.";
+  }
+
+  let upperCaseWord = word.toUpperCase();
+
+  const horizontalString = letters.map(ls => ls.join(''));
+  for (let line of horizontalString) {
+    if (line.includes(upperCaseWord)) {
+      return true;
+    }
+  }
+
+  let arrayOfColumns = [];
+  for (let i = 0; i < letters.length; i++) {
+    let columns = [];
+    for (let j = 0; j < letters.length; j++) {
+      columns.push(letters[j][i]);
+    }
+    arrayOfColumns.push(columns);
+  }
+
+  let columnString = [];
+  for (let element of arrayOfColumns) {
+    columnString.push(element.join(''));
+  }
+
+  for (let line of columnString) {
+    if (line.includes(upperCaseWord)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
